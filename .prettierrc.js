fae81b747dfc3662295f1587b313cb4ad9bc6cc3
banri <banri.yanahama@gmail.com>
2024-07-30 16:07:58 +0900

    add prettier and update eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'auto',
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+};
