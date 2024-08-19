253d0903257e438b3dfcf5a91f37cfebc4c87048
banri <banri.yanahama@gmail.com>
2024-08-19 15:03:15 +0900

    update form #5

--- src/components/intent-order-form.tsx
+++ src/components/intent-order-form.tsx
@@ -1,6 +1,6 @@
 // components/intent-order-form.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { z } from 'zod';
 
 import { ChainId } from '@/types/chain-id';
@@ -10,9 +10,9 @@ import { Input } from './ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 
 const formSchema = z.object({
-  inputToken: z.enum(['ETH', 'USDC', 'USDT', 'DAI']),
+  inputToken: z.string(),
   inputAmount: z.number(),
-  outputToken: z.enum(['ETH', 'USDC', 'USDT', 'DAI']),
+  outputToken: z.string(),
   outputStartAmount: z.number(),
   outputEndAmount: z.number(),
 });
@@ -20,11 +20,11 @@ const formSchema = z.object({
 const IntentOrderForm = (props: { chainId: ChainId }) => {
   const { chainId } = props;
 
-  const [inputToken, setInputToken] = React.useState('ETH');
-  const [inputAmount, setInputAmount] = React.useState(0.1);
-  const [outputToken, setOutputToken] = React.useState('USDC');
-  const [outputStartAmount, setOutputStartAmount] = React.useState(200);
-  const [outputEndAmount, setOutputEndAmount] = React.useState(100);
+  const [inputToken, setInputToken] = useState('ETH');
+  const [inputAmount, setInputAmount] = useState(0.1);
+  const [outputToken, setOutputToken] = useState('USDC');
+  const [outputStartAmount, setOutputStartAmount] = useState(200);
+  const [outputEndAmount, setOutputEndAmount] = useState(100);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
@@ -32,8 +32,14 @@ const IntentOrderForm = (props: { chainId: ChainId }) => {
 
   return (
     <div className='flex flex-row gap-2 items-center'>
-      <Input className='w-24 text-right' placeholder='100' type='number' />
-      <Select>
+      <Input
+        className='w-24 text-right'
+        placeholder='100'
+        type='number'
+        value={inputAmount}
+        onChange={(e) => setInputAmount(Number(e.target.value))}
+      />
+      <Select onValueChange={setInputToken}>
         <SelectTrigger className='w-18'>
           <SelectValue placeholder='Token' />
         </SelectTrigger>
@@ -46,11 +52,21 @@ const IntentOrderForm = (props: { chainId: ChainId }) => {
       </Select>
       <div>{'->'}</div>
       <div className='flex flex-row gap-1 items-center'>
-        <Input className='w-24 text-right' placeholder='50' />
+        <Input
+          className='w-24 text-right'
+          placeholder='50'
+          value={outputStartAmount}
+          onChange={(e) => setOutputStartAmount(Number(e.target.value))}
+        />
         <div>{'~'}</div>
-        <Input className='w-24 text-right' placeholder='10' />
+        <Input
+          className='w-24 text-right'
+          placeholder='10'
+          value={outputEndAmount}
+          onChange={(e) => setOutputEndAmount(Number(e.target.value))}
+        />
       </div>
-      <Select>
+      <Select onValueChange={setOutputToken}>
         <SelectTrigger className='w-18'>
           <SelectValue placeholder='Token' />
         </SelectTrigger>
@@ -62,13 +78,14 @@ const IntentOrderForm = (props: { chainId: ChainId }) => {
         </SelectContent>
       </Select>
       <Button
+        className='rounded-2xl drop-shadow-md font-bold text-md'
         onClick={() =>
           onSubmit({
-            inputToken: 'ETH',
-            inputAmount: 0.1,
-            outputToken: 'USDC',
-            outputStartAmount: 200,
-            outputEndAmount: 100,
+            inputToken,
+            inputAmount,
+            outputToken,
+            outputStartAmount,
+            outputEndAmount,
           })
         }
       >
