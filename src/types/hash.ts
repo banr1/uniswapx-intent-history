f97fb4c6655196f73d7f666289e7eaf71d2baee0
banri <banri.yanahama@gmail.com>
2024-08-22 20:16:24 +0900

    update fetching tx and add fetching fill event #7

--- src/types/hash.ts
+++ src/types/hash.ts
@@ -4,4 +4,5 @@ export type Hash = TxHash | IntentHash | Address;
 export type TxHash = string;
 export type IntentHash = string;
 export type Address = string;
+export type ContractAddress = Address;
 export type ShortHash = string;
